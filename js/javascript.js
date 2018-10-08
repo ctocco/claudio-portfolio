318acb04de74463bc3e97f9567f881f66edc3a36
Claudio Tocco <tocco.a.claudio@gmail.com>
2018-10-08 07:25:08 +0200

    added resume

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,9 @@
-$(window).on("scroll", function() {
-  if ($(window).scrollTop() > 50) {
-    $(".nav-block").addClass("active");
-  } else {
-    //remove the background property so it comes transparent again (defined in your css)
-    $(".nav-block").removeClass("active");
-  }
-});
-
 $(document).ready(function() {
   var scrollLink = $(".scroll");
+
+  $(window).on("beforeunload", function() {
+    $(window).scrollTop(0);
+  });
 
   // smooth scrolling
   scrollLink.click(function(e) {
@@ -20,4 +15,13 @@ $(document).ready(function() {
       1000
     );
   });
+});
+
+$(window).on("scroll", function() {
+  if ($(window).scrollTop() > 50) {
+    $(".nav-block").addClass("active");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $(".nav-block").removeClass("active");
+  }
 });
